a6b84f7a64ce4ee2c51d2516cd5da4b55c6e5dc8
Matheus Marques <115881128+MathMNunes@users.noreply.github.com>
2023-05-22 11:37:35 -0300

    Add files via upload

---
+++ Parte 10 - JavaScript tipos, variáveis e funções/type-bboolean.js
@@ -0,0 +1,13 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let cadastroAtividado = false;
+
+//comparação
+
+console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
---
+++ Parte 10 - JavaScript tipos, variáveis e funções/type-strings.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "O Leo disse'oi!'";
+const meuNome = "Matheus";
+console.log(citacao);
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// template string ou template literral
