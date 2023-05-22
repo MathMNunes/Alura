f8c9a978cc2bf18cd051e013a0a1f3643356d044
Matheus Marques <115881128+MathMNunes@users.noreply.github.com>
2023-05-22 16:06:13 -0300

    Add files via upload

---
+++ Parte 10 - JavaScript tipos, variáveis e funções/conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+
+//Number()
+//String()
+//console.log(numero == numeroString)
+//console.log(numero + numeroString)
+console.log(numero + Number(numeroString))
+
+
+// conversão explícita
+
---
+++ Parte 10 - JavaScript tipos, variáveis e funções/script.js
@@ -0,0 +1,36 @@
+//==============var===============
+
+// var altura = 5;
+// var comprimento = 7;
+
+// var area = altura  *  comprimento;
+// console.log(area);
+
+//=================let=================
+// let forma = 'retangulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retangulo') {
+//     area = altura * comprimento;
+// } else{
+//     area = (altura * comprimento)/2;
+// }
+
+// console.log(area)
+
+// ===================const===============
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else{
+    area = (altura * comprimento)/2;
+}
+
+console.log(area)
