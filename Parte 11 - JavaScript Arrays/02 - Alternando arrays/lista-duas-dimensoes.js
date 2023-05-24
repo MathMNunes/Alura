f5774210c85f463c6823019f8a9861c8f6548fef
Matheus Marques <115881128+MathMNunes@users.noreply.github.com>
2023-05-24 00:31:33 -0300

    Add files via upload

---
+++ Parte 11 - JavaScript Arrays/02 - Alternando arrays/lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+const mediasDosAlunos = [10,7,9,6]
+
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, sua media é ${listaDeNotasEAlunos[1][0]} `);
